2380c5b298bdefcb89ad386ec03eecb585c67951
Eric Ng <naclericny@gmail.com>
2020-08-16 17:53:04 -0400

    turned render_md into a method

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import webbrowser
 import markdown
 import os
 
 
-# TODO: convert current_directory into instance variable
-# TODO: fix this fking bullshit
 class RouteHandler(BaseHTTPRequestHandler):
     current_directory = os.curdir
-    STYLE = '<link rel="STYLEsheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css">'
+    STYLE = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css">'
 
     def do_GET(self):
         self.send_response(200)
@@ -21,14 +20,16 @@ class RouteHandler(BaseHTTPRequestHandler):
             if os.path.isdir(new_directory):
                 self.wfile.write(self.render_file_list().encode())
             else:
-                self.wfile.write(render_md(new_directory).encode())
+                self.wfile.write(self.render_md(new_directory).encode())
 
     # Entry point
     def render_table_of_contents(self) -> str:
-        table_of_contents = self.STYLE 
+        table_of_contents = self.STYLE
         for directory in os.listdir(self.current_directory):
             if os.path.isdir(directory):
-                table_of_contents += f"<h1><a href='{directory}'> /{directory} </a></h1>"
+                table_of_contents += (
+                    f"<h1><a href='{directory}'> /{directory} </a></h1>"
+                )
         return table_of_contents
 
     def render_file_list(self) -> str:
@@ -43,24 +44,25 @@ class RouteHandler(BaseHTTPRequestHandler):
                 md_file_list += f"<h1><a href='{self.path}'> /{item} </a></h1>"
         return md_file_list
 
+    def render_md(self, file: str) -> str:
+        html = self.STYLE
+        with open(file, "r", encoding="utf-8") as md:
+            text = md.read()
+        html += markdown.markdown(text)
+        return html
+
     # If something gets clicked, recursively find all the other directories
     def update_directory(self) -> str:
         self.current_directory = f".{self.path}"
         return self.current_directory
 
 
-def render_md(file: str) -> str:
-    # fix the dir
-    with open(file, "r", encoding="utf-8") as md:
-        text = md.read()
-    html = markdown.markdown(text)
-    return html
-
-
 def run_server(handler_class=RouteHandler) -> None:
+    """ Runs the server and opens in web browser """
     server_address = ("", 8000)
     http_server = HTTPServer(server_address, handler_class)
     print(f"Running on Port: {server_address[1]}")
+    webbrowser.open(f"localhost:{server_address[1]}")
     http_server.serve_forever()
 
 
