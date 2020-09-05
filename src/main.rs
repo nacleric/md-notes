3f0617bf3ebb168697dffd03d44302577adc2b23
Eric Ng <naclericny@gmail.com>
2020-09-05 12:47:13 -0400

    added brads changes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use pulldown_cmark::{html, Parser};
 use std::path::PathBuf;
+
+use pulldown_cmark::{html, Parser};
 use structopt::StructOpt;
 use tiny_http::{Response, Server};
+use webbrowser;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -10,66 +12,55 @@ struct Cli {
     path: PathBuf,
 }
 
-#[derive(Debug)]
-enum Mdfile {
-    IsMd,
-    NotMd,
-}
-
-// Checks if file is a markdown file
-#[allow(dead_code)]
-fn check_for_mdfile(file: &PathBuf) -> Mdfile {
-    //let expected_filetype: &str = file.to_str().unwrap(); Don't be tempted to use unwrap()
-    let option = file.to_str();
-    let expected_filetype = match option {
-        Some(s) => match &s[&s.len() - 2..] {
-            "md" => Mdfile::IsMd,
-            _ => Mdfile::NotMd,
-        },
-        None => panic!("Could not convert string"),
-    };
-    expected_filetype
+fn is_md_file<P>(path: P) -> bool
+where
+    P: Into<PathBuf>,
+{
+    path.into() // Into<PathBuf>::into() -> PathBuf
+        .extension() // PathBuf::extension() -> Option<&OsStr>
+        .is_some() // Option<&OsStr> -> bool
 }
 
 fn parse_arg_to_md() -> String {
     let args = Cli::from_args();
-    let mdfile = check_for_mdfile(&args.path);
+    let mdfile = is_md_file(&args.path);
 
-    match mdfile {
-        Mdfile::IsMd => {
-            let result = std::fs::read_to_string(&args.path);
-            let content = match result {
-                Ok(content) => content,
-                Err(error) => {
-                    panic!("Can't deal with {:?}, just exit here", error);
-                }
-            };
-            let parser = Parser::new(&content);
-            let mut html_output = String::new();
-            html::push_html(&mut html_output, parser);
+    if mdfile {
+        let result = std::fs::read_to_string(&args.path);
+        let content = match result {
+            Ok(content) => content,
+            Err(error) => {
+                panic!("Can't deal with {:?}, just exit here", error);
+            }
+        };
+        let parser = Parser::new(&content);
+        let mut html_output = String::new();
+        html::push_html(&mut html_output, parser);
 
-            html_output
-        },
-        Mdfile::NotMd => {
-            panic!("Not a Markdown file");
-        },
+        html_output
+    } else {
+        panic!("Not a Markdown file");
     }
 }
 
 fn main() {
+    let mut html_body = String::from("<link rel='stylesheet' href='https://unpkg.com/sakura.css/css/sakura.css' type='text/css'>");
     let html_output = parse_arg_to_md();
+    html_body.push_str(&html_output);
 
     let server = Server::http("0.0.0.0:8000");
     println!("Port: 8000, Server is running...");
+    webbrowser::open("http://localhost:8000/").unwrap();
+
     match server {
         Ok(server) => loop {
             // Prints requests that hit the server
             match server.recv() {
-                Ok(req) => {
-                    println!("{:?}", req);
-                    let response = Response::from_data(html_output.clone().into_bytes());
-                    req.respond(response).unwrap();
-                },
+                Ok(request) => {
+                    println!("{:?}", request);
+                    let response = Response::from_data(html_body.clone().into_bytes());
+                    request.respond(response).unwrap();
+                }
                 Err(error) => {
                     panic!("Error: {:?}", error);
                 }
